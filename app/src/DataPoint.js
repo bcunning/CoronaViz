23f4e97c78591e68d6f50622087b9810a4694023
Ben Cunningham <bcunning@cs.stanford.edu>
2020-06-28 06:42:24 -0400

    Make datapoint consistent with shimmed array

--- app/src/DataPoint.js
+++ app/src/DataPoint.js
@@ -20,7 +20,6 @@ Object.defineProperty(arrayPrototype, "rawdata", {
         return this;
     },
     set: function rawdata(newData) {
-        console.log("setting data");
     }
 });
 Object.defineProperty(arrayPrototype, "value", {
@@ -48,24 +47,24 @@ export default class DataPoint {
         if (Number.isNaN(value) || value === undefined) {
             value = 0;
         }
-        this.data = new Array(13).fill(0);
+        this.rawdata = new Array(13).fill(0);
         this.value = value;
     }
 
     get value() {
-        return this.data[DataPoint.FilterIndexToUsedIndices(StatIndex.Value)];
+        return this.rawdata[DataPoint.FilterIndexToUsedIndices(StatIndex.Value)];
     }
 
     set value(newValue) {
-        this.data[DataPoint.FilterIndexToUsedIndices(StatIndex.Value)] = newValue;
+        this.rawdata[DataPoint.FilterIndexToUsedIndices(StatIndex.Value)] = newValue;
     }
 
     get change() {
-        return this.data[DataPoint.FilterIndexToUsedIndices(StatIndex.Change)];
+        return this.rawdata[DataPoint.FilterIndexToUsedIndices(StatIndex.Change)];
     }
 
     set change(newChange) {
-        this.data[DataPoint.FilterIndexToUsedIndices(StatIndex.Change)] = newChange;
+        this.rawdata[DataPoint.FilterIndexToUsedIndices(StatIndex.Change)] = newChange;
     }
 
     static FilterIndexToUsedIndices(index) {
